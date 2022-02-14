24473f644184dfaa0dee19b1b8e428cefc50babf
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2022-02-14 22:49:48 +0100

    colors: new feature for easy ANSI color and text effect application on terminals

---
+++ include/cosmos/io/colors.hxx
@@ -0,0 +1,266 @@
+#ifndef COSMOS_COLORS_HXX
+#define COSMOS_COLORS_HXX
+
+// stdlib
+#include <iosfwd>
+#include <variant>
+
+/** \file
+ * This header provides ANSI terminal features for printing colored or
+ * otherwise marked up text. The ostream operators work only for terminal
+ * devices, see Terminal::isTTY().
+ *
+ * This is in a separate namespace due to a lot of symbol pollution. Since a
+ * lot of types are in here a `using namespace cosmos::term` can help making
+ * code more compact by * importing this limited namespace.
+ **/
+
+namespace cosmos::term {
+
+/// Primitive Colors for ANSI Terminals.
+/**
+ *  There are 16 differen colors when taking bright variants into account.
+ *  The colors can be used both for text color and background color.
+ **/
+enum class TermColor : size_t {
+	// the integer values denote the offset from the base ANSI escape code
+	// for front/back bright/normal colors.
+	BLACK = 0,
+	RED,
+	GREEN,
+	YELLOW,
+	BLUE,
+	MAGENTA,
+	CYAN,
+	WHITE
+};
+
+/// Differentiation between text (front) and background color.
+enum class ColorKind {
+	FRONT,
+	BACK
+};
+
+/// Differentiation of normal and bright color intensity.
+enum class ColorIntensity {
+	NORMAL,
+	BRIGHT
+};
+
+/// Complete color specification for ANSI terminals.
+/**
+ *  This type carries a complete color specification:
+ *
+ *  - basic color value
+ *  - FRONT or BACK color
+ *  - NORMAL or BRIGHT intensity
+ *
+ *  It can be used to permanently change the color setting of the terminal
+ *  and is also used for FeatureBase to build more complex color
+ *  mechanisms.
+ **/
+class ColorSpec {
+public: // functions
+
+	ColorSpec(const TermColor &color, const ColorKind &kind, const ColorIntensity &intensity) :
+		m_color(color), m_kind(kind), m_intensity(intensity) {}
+
+	TermColor getColor() const { return m_color; }
+	bool isBright() const { return m_intensity == ColorIntensity::BRIGHT; }
+	bool isNormal() const { return !isBright(); }
+	bool isFrontColor() const { return m_kind == ColorKind::FRONT; }
+	bool isBackColor() const { return !isFrontColor(); }
+
+protected: // data
+	TermColor m_color;
+	ColorKind m_kind = ColorKind::FRONT;
+	ColorIntensity m_intensity = ColorIntensity::NORMAL;
+};
+
+/// Simple type to represent an ANSI foreground color in bright or normal intensity.
+struct FrontColor : public ColorSpec {
+	explicit FrontColor(const TermColor &c) : ColorSpec(c, ColorKind::FRONT, ColorIntensity::NORMAL) {}
+	FrontColor& setBright() { m_intensity = ColorIntensity::BRIGHT; return *this; }
+};
+
+/// Simple type to represent an ANSI background color in bright or normal intensity.
+struct BackColor : public ColorSpec {
+	explicit BackColor(const TermColor &c) : ColorSpec(c, ColorKind::BACK, ColorIntensity::NORMAL) {}
+	BackColor& setBright() { m_intensity = ColorIntensity::BRIGHT; return *this; }
+};
+
+/// Various feature controls for ANSI terminals.
+enum class TermControl : size_t {
+	RESET = 0, //! remove all attributes currently set (including colors)
+	UNDERLINE_ON = 4, //! underlined text
+	UNDERLINE_OFF = 24,
+	BLINK_ON = 5, //! blinking text
+	BLINK_OFF = 25,
+	INVERSE_ON = 7, //! inverse fg/bg colors
+	INVERSE_OFF = 27,
+	DEFAULT_FG_COLOR = 39, //! set default fg color
+	DEFAULT_BG_COLOR = 49 //! set default bg color
+};
+
+/// Returns the matching _OFF value for an _ON value of the TermControl enum.
+/**
+ * This can throw an exception on unexpected input.
+ **/
+TermControl getOffControl(const TermControl &ctrl);
+/// Returns the actual ANSI escape code number for the given color specification.
+size_t getANSIColorCode(const ColorSpec &color);
+
+/// Base class to build nested ANSI feature objects.
+/**
+ *  Each instance of this object either holds a piece of text or a pointer to
+ *  the next feature to be applied. Each object at this level only knows the
+ *  ANSI codes to enable and disable the feature it represents.
+ *
+ *  The output operator thus can apply all desired features for the piece of
+ *  text and restore the original terminal features after the output is
+ *  finished.
+ *
+ *  This allows nested features like
+ *      std::cout << Green(OnBlue("my green on //blue text")) << "\n";
+ **/
+class FeatureBase {
+public:
+	size_t getOnCode() const { return m_on_code; }
+	size_t getOffCode() const { return m_off_code; }
+	bool hasText() const { return std::holds_alternative<const std::string*>(m_info); }
+	const std::string& getText() const { return *std::get<const std::string*>(m_info); }
+	bool hasNextFeature() const { return std::holds_alternative<const FeatureBase*>(m_info); }
+	const FeatureBase& getNextFeature() const { return *std::get<const FeatureBase*>(m_info); }
+protected:
+	FeatureBase(const std::string &text, const size_t on_code, const size_t off_code) :
+		m_info(&text), m_on_code(on_code), m_off_code(off_code)
+	{}
+
+	FeatureBase(const FeatureBase &next, const size_t on_code, const size_t off_code) :
+		m_info(&next), m_on_code(on_code), m_off_code(off_code)
+	{}
+protected:
+	// either a terminal string or a pointer to the next feature to apply
+	std::variant<const std::string*, const FeatureBase*> m_info;
+	const size_t m_on_code;
+	const size_t m_off_code;
+};
+
+/// Base class for easy feature TermControl application on ostreams.
+class TextEffect : public FeatureBase {
+protected:
+	TextEffect(const TermControl feature, const std::string &text) :
+		FeatureBase(text, static_cast<size_t>(feature), static_cast<size_t>(getOffControl(feature)))
+	{}
+
+	TextEffect(const TermControl feature, const FeatureBase &next) :
+		FeatureBase(next, static_cast<size_t>(feature), static_cast<size_t>(getOffControl(feature)))
+	{}
+};
+
+/// Template for definition of concrete text effect helpers.
+template <TermControl effect>
+struct TextEffectT : public TextEffect {
+	explicit TextEffectT(const std::string &text) :
+		TextEffect(effect, text) {}
+	explicit TextEffectT(const FeatureBase &next) :
+		TextEffect(effect, next) {}
+};
+
+/// Helper to print underlined text easily onto an ostream.
+typedef TextEffectT<TermControl::UNDERLINE_ON> Underlined;
+/// Helper to print blinking text easily onto an ostream.
+typedef TextEffectT<TermControl::BLINK_ON> Blinking;
+/// Helper to print blinking text easily onto an ostream.
+typedef TextEffectT<TermControl::INVERSE_ON> Inversed;
+
+/// Base class for easy colored text application on ostreams.
+struct ColoredText : public FeatureBase {
+	explicit ColoredText(const std::string &text, const TermColor &c, const ColorKind &kind, const ColorIntensity &intensity) :
+		FeatureBase(text, getANSIColorCode(ColorSpec(c, kind, intensity)), static_cast<size_t>(getOffCode(kind)))
+	{}
+
+	explicit ColoredText(const FeatureBase &next, const TermColor &c, const ColorKind &kind, const ColorIntensity &intensity) :
+		FeatureBase(next, getANSIColorCode(ColorSpec(c, kind, intensity)), static_cast<size_t>(getOffCode(kind)))
+	{}
+
+protected: // functions
+
+	TermControl getOffCode(const ColorKind &kind) const {
+		return kind == ColorKind::FRONT ? TermControl::DEFAULT_FG_COLOR : TermControl::DEFAULT_BG_COLOR;
+	}
+};
+
+/// Template for definition of concrete color text helpers.
+template <TermColor color, ColorIntensity intensity=ColorIntensity::NORMAL>
+struct ColoredTextT : public ColoredText {
+	explicit ColoredTextT(const std::string &text) : ColoredText(text, color, ColorKind::FRONT, intensity) {}
+	explicit ColoredTextT(const FeatureBase &next) : ColoredText(next, color, ColorKind::FRONT, intensity) {}
+};
+
+/// Template for definition of concrete background color helpers.
+template <TermColor color, ColorIntensity intensity=ColorIntensity::NORMAL>
+struct TextOnColorT : public ColoredText {
+	explicit TextOnColorT(const std::string &text) : ColoredText(text, color, ColorKind::BACK, intensity) {}
+	explicit TextOnColorT(const FeatureBase &next) : ColoredText(next, color, ColorKind::BACK, intensity) {}
+};
+
+/*
+ * Helpers to apply colored text and background easily on  an ostream.
+ *
+ * These only enable the color for the given string argument and disable
+ * coloring after the output operation again.
+ *
+ * These helpers can be nested with text effects or background helpers to
+ * create readable chained effects for a string like:
+ *
+ * std::cout << Green(OnWhite(Underlined("text"))) << std::endl;
+ */
+
+typedef ColoredTextT<TermColor::BLACK> Black;
+typedef ColoredTextT<TermColor::BLACK, ColorIntensity::BRIGHT> BrightBlack;
+typedef TextOnColorT<TermColor::BLACK> OnBlack;
+typedef TextOnColorT<TermColor::BLACK, ColorIntensity::BRIGHT> OnBrightBlack;
+
+typedef ColoredTextT<TermColor::RED> Red;
+typedef ColoredTextT<TermColor::RED, ColorIntensity::BRIGHT> BrightRed;
+typedef TextOnColorT<TermColor::RED> OnRed;
+typedef TextOnColorT<TermColor::RED, ColorIntensity::BRIGHT> OnBrightRed;
+
+typedef ColoredTextT<TermColor::GREEN> Green;
+typedef ColoredTextT<TermColor::GREEN, ColorIntensity::BRIGHT> BrightGreen;
+typedef TextOnColorT<TermColor::GREEN> OnGreen;
+typedef TextOnColorT<TermColor::GREEN, ColorIntensity::BRIGHT> OnBrightGreen;
+
+typedef ColoredTextT<TermColor::YELLOW> Yellow;
+typedef ColoredTextT<TermColor::YELLOW, ColorIntensity::BRIGHT> BrightYellow;
+typedef TextOnColorT<TermColor::YELLOW> OnYellow;
+typedef TextOnColorT<TermColor::YELLOW, ColorIntensity::BRIGHT> OnBrightYellow;
+
+typedef ColoredTextT<TermColor::BLUE> Blue;
+typedef ColoredTextT<TermColor::BLUE, ColorIntensity::BRIGHT> BrightBlue;
+typedef TextOnColorT<TermColor::BLUE> OnBlue;
+typedef TextOnColorT<TermColor::BLUE, ColorIntensity::BRIGHT> OnBrightBlue;
+
+typedef ColoredTextT<TermColor::MAGENTA> Magenta;
+typedef ColoredTextT<TermColor::MAGENTA, ColorIntensity::BRIGHT> BrightMagenta;
+typedef TextOnColorT<TermColor::MAGENTA> OnMagenta;
+typedef TextOnColorT<TermColor::MAGENTA, ColorIntensity::BRIGHT> OnBrightMagenta;
+
+typedef ColoredTextT<TermColor::CYAN> Cyan;
+typedef ColoredTextT<TermColor::CYAN, ColorIntensity::BRIGHT> BrightCyan;
+typedef TextOnColorT<TermColor::CYAN> OnCyan;
+typedef TextOnColorT<TermColor::CYAN, ColorIntensity::BRIGHT> OnBrightCyan;
+
+typedef ColoredTextT<TermColor::WHITE> White;
+typedef ColoredTextT<TermColor::WHITE, ColorIntensity::BRIGHT> BrightWhite;
+typedef TextOnColorT<TermColor::WHITE> OnWhite;
+typedef TextOnColorT<TermColor::WHITE, ColorIntensity::BRIGHT> OnBrightWhite;
+
+} // end ns
+
+std::ostream& operator<<(std::ostream &o, const cosmos::term::ColorSpec &fc);
+std::ostream& operator<<(std::ostream &o, const cosmos::term::TermControl &p);
+std::ostream& operator<<(std::ostream &o, const cosmos::term::FeatureBase &fb);
+
+#endif // inc. guard
---
+++ src/io/colors.cxx
@@ -0,0 +1,100 @@
+// stdlib
+#include <cassert>
+#include <ostream>
+#include <vector>
+
+// cosmos
+#include "cosmos/errors/UsageError.hxx"
+#include "cosmos/io/colors.hxx"
+
+namespace cosmos::term {
+
+static std::string buildANSICommand(const std::vector<size_t> &cmd_list) {
+	// the ASCII escape character for ANSI control sequences
+	constexpr char ANSI_ESC_START = '\033';
+	// ANSI escape finish character
+	constexpr char ANSI_ESC_FINISH = 'm';
+
+	std::string ret(1, ANSI_ESC_START);
+	ret += '[';
+	for (const auto &cmd: cmd_list) {
+		ret += std::to_string(cmd);
+		ret += ";";
+	}
+	// remove superfluous separator again
+	ret.pop_back();
+	ret += ANSI_ESC_FINISH;
+	return ret;
+}
+
+static std::string buildANSICommand(size_t code) {
+	return buildANSICommand(std::vector<size_t>({code}));
+}
+
+size_t getANSIColorCode(const ColorSpec &color) {
+	size_t code = color.isFrontColor() ? 30 : 40;
+	if (color.isBright())
+		code += 60;
+
+	return code + static_cast<size_t>(color.getColor());
+}
+
+TermControl getOffControl(const TermControl &ctrl) {
+	switch(ctrl)
+	{
+	case TermControl::UNDERLINE_ON: return TermControl::UNDERLINE_OFF;
+	case TermControl::BLINK_ON: return TermControl::BLINK_OFF;
+	case TermControl::INVERSE_ON: return TermControl::INVERSE_OFF;
+	default: cosmos_throw (UsageError("bad value for getOffControl()"));
+		 return TermControl::RESET; /* to silence compiler warning */
+	}
+}
+
+static std::vector<const FeatureBase*> getFeatures(const FeatureBase &first) {
+	std::vector<const FeatureBase*> ret;
+
+	const FeatureBase *feature = &first;
+	while (true) {
+		ret.push_back(feature);
+
+		if (!feature->hasNextFeature())
+			break;
+
+		feature = &feature->getNextFeature();
+	}
+
+	return ret;
+}
+
+} // end ns
+
+
+std::ostream& operator<<(std::ostream &o, const cosmos::term::ColorSpec &fc) {
+	const size_t code = getANSIColorCode(fc);
+
+	return o << cosmos::term::buildANSICommand(code);
+}
+
+std::ostream& operator<<(std::ostream &o, const cosmos::term::TermControl &p) {
+	return o << cosmos::term::buildANSICommand(static_cast<size_t>(p));
+}
+
+std::ostream& operator<<(std::ostream &o, const cosmos::term::FeatureBase &fb) {
+	using namespace cosmos::term;
+
+	// simple case without dynamic allocation
+	if (!fb.hasNextFeature()) {
+		return o << buildANSICommand(fb.getOnCode()) << fb.getText() << buildANSICommand(fb.getOffCode());
+	}
+
+	// complex case with nested features
+	std::vector<size_t> on_codes, off_codes;
+	const auto features = getFeatures(fb);
+	for (const auto feature: features) {
+		on_codes.push_back(feature->getOnCode());
+		off_codes.push_back(feature->getOffCode());
+	}
+
+	assert (features.back()->hasText());
+	return o << buildANSICommand(on_codes) << features.back()->getText() << buildANSICommand(off_codes);
+}
