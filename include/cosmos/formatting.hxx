6982492ac0078bc57b6c78cb336aa7c0cf0e24f8
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-01-26 18:17:09 +0100

    formatting: prevent bad hexnum usage and fix output of char types
    - the static_assert will prevent using hexnum with non-integral types
    - the to_printable_integer transparently fixes the output of char
      related types

--- include/cosmos/formatting.hxx
+++ include/cosmos/formatting.hxx
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <ostream>
 #include <string>
+#include <type_traits>
 
 // cosmos
 #include "cosmos/compiler.hxx"
@@ -18,8 +19,6 @@ namespace cosmos {
  **/
 template <typename NUM>
 struct hexnum {
-	// TODO: BUG: if NUM is a `char` type then this breaks and outputs
-	// characters instead of numbers
 	explicit hexnum(const NUM &num, size_t width) : m_num(num), m_width(width) {}
 
 	/// if we should prefix '0x' to the number (default yes)
@@ -39,12 +38,27 @@ protected: // data
 /// this is a C++ variant of the libc sprintf() function
 COSMOS_API std::string sprintf(const char *fmt, ...) COSMOS_FORMAT_PRINTF(1, 2);
 
+/// this helper makes sure that any integer is turned into a printable integer
+/**
+ * Attempting to output a `char` related type onto an ostream will print its
+ * symbolic value as opposed to its numerical representation. To avoid this
+ * effect this helper function can be used to return a representation of num
+ * that will be printed as a numerical value when output onto an ostream.
+ **/
+template <typename T>
+auto to_printable_integer(T num) -> decltype(+num) {
+	// using the uniary + operator promotes the number to a printable type
+	return +num;
 }
+
+} // end ns cosmos
 
 template <typename NUM>
 std::ostream& operator<<(std::ostream &o, const cosmos::hexnum<NUM> &hn) {
 	const auto orig_flags = o.flags();
 	const auto orig_fill = o.fill();
+
+	static_assert(std::is_integral_v<NUM>, "template type needs to be an integral integer type");
 
 	// don't handle this with std::showbase, it's behaving badly e.g. the
 	// "0x" prefix counts towards the field with, also, the fill character
@@ -52,7 +66,8 @@ std::ostream& operator<<(std::ostream &o, const cosmos::hexnum<NUM> &hn) {
 	// "0000x64".
 	if (hn.showBase())
 		o << "0x";
-	o << std::setw(hn.getWidth()) << std::hex << std::setfill('0') << hn.getNum();
+	o << std::setw(hn.getWidth()) << std::hex << std::setfill('0')
+		<< cosmos::to_printable_integer(hn.getNum());
 
 	o.flags(orig_flags);
 	o.fill(orig_fill);
