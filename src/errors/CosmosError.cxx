8262b243ea453cbc013172405708b35fb2bf643f
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-02-13 00:32:06 +0100

    CosmosError: fix message content set at construction time (UsageError)
    Somewhat a bug was introduced that causes message content already set at
    construction time, like the case wit UsageError, to be left at the
    beginning of the message, instead of at the end.
    
    Fix this by remembering the original message content and appending it
    after the base information has been applied.

--- src/errors/CosmosError.cxx
+++ src/errors/CosmosError.cxx
@@ -8,10 +8,12 @@ namespace cosmos {
 
 const char* CosmosError::what() const throw() {
 	if (!m_msg_generated) {
+		auto orig = m_msg;
 		std::stringstream ss;
 		ss << m_file << ":" << m_line << " [" << m_func << "]: "
 			<< m_error_class << ": ";
-		m_msg += ss.str();
+		m_msg = ss.str();
+		m_msg += orig;
 		generateMsg();
 		m_msg_generated = true;
 	}
