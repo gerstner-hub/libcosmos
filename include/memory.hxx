791da9301620a440fc993ae24f165a4c0315cbc1
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2023-02-21 20:31:39 +0100

    memory: new header offering memory related helpers

---
+++ include/memory.hxx
@@ -0,0 +1,23 @@
+#ifndef COSMOS_MEMORY_HXX
+#define COSMOS_MEMORY_HXX
+
+// C++
+#include <cstring>
+#include <type_traits>
+
+namespace cosmos {
+
+/// Completely overwrites the given object with zeroes
+/**
+ * This is typically used in C-APIs to get a defined object state.
+ **/
+template <typename T>
+void zero_object(T &obj) {
+	// this should only be done for non-C++ types
+	static_assert(std::is_trivial<T>::value == true);
+	std::memset(&obj, 0, sizeof(T));
+}
+
+} // end ns
+
+#endif // inc. guard
