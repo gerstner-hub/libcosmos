1a60b901385cff5a47d38b0c63806458b1df173b
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2024-08-04 14:24:29 +0200

    ptrace: add enum for the request argument

--- include/proc/ptrace.hxx
+++ include/proc/ptrace.hxx
@@ -67,4 +67,82 @@ enum class TraceEvent {
 	STOP       = PTRACE_EVENT_STOP
 };
 
+/// Basic request types that can be passed to the ptrace() system call
+/**
+ * \note On system call level this has an actual `enum __ptrace_request` type,
+ *       thus there is no defined underlying type and we keep the compiler's
+ *       default.
+ **/
+enum class TraceRequest {
+	/// The tracee asks to be traced by its parent.
+	TRACEME     = PTRACE_TRACEME,
+	/// Read a word at a given address of the tracee's memory.
+	PEEKDATA    = PTRACE_PEEKDATA,
+	/// Read a word at a given offset of the tracee's USER data (holds registers and other metadata).
+	PEEKUSER    = PTRACE_PEEKUSER,
+	/// Write a word at the given address into the tracee's memory.
+	POKEDATA    = PTRACE_POKEDATA,
+	/// Write a word at the given offset into the tracee's USER data.
+	POKEUSER    = PTRACE_POKEUSER,
+	/// Copy the tracee's general-purpose registers to the given address in the tracer.
+	GETREGS     = PTRACE_GETREGS,
+	/// Copy the tracee's floating-point registers to the given address in the tracer.
+	GETFPREGS   = PTRACE_GETFPREGS,
+	/// Reg the tracee's registers in an architecture dependent way.
+	GETREGSET   = PTRACE_GETREGSET,
+	/// Modify the tracee's general-purpose registers (not available on all architectures).
+	SETREGS     = PTRACE_SETREGS,
+	/// Modify the tracee's floating-point registers.
+	SETFPGREGS  = PTRACE_SETFPREGS,
+	/// Modify the tracee's registers, analogous to GETREGSET.
+	SETREGSET   = PTRACE_SETREGSET,
+	/// Retrieve information about the signal that cause the tracee to stop. Copies a siginfo_t structure.
+	GETSIGINFO  = PTRACE_GETSIGINFO,
+	/// Modify the tracee's signal information (used when the tracer catched a signal that would normally be delivered to the tracee).
+	SETSIGINFO  = PTRACE_SETSIGINFO,
+	/// Retrieve siginfo_t structures without removing them from the tracee's queue.
+	PEEKSIGINFO = PTRACE_PEEKSIGINFO,
+	/// Retrieves a copy of the mask of blocked signals from the tracee.
+	GETSIGMASK  = PTRACE_GETSIGMASK,
+	/// Change the tracee's mask of blocked signals.
+	SETSIGMASK  = PTRACE_SETSIGMASK,
+	/// Set ptrace options (see TraceFlags)
+	SETOPTIONS  = PTRACE_SETOPTIONS,
+	/// Retrieve a message (unsigned long) about the ptrace event that just happened.
+	GETEVENTMSG = PTRACE_GETEVENTMSG,
+	/// Restart the stopped tracee. Non-zero data is interpreted as a signal number to be delivered to the tracee.
+	CONT        = PTRACE_CONT,
+	/// Restart the stopped tracee like CONT, but arrange for the tracee to be stopped at entry/exit to/from a system call.
+	SYSCALL     = PTRACE_SYSCALL,
+	/// Like SYSCALL, but arrange for the tracee to be stopped after a single instruction.
+	SINGLESTEP  = PTRACE_SINGLESTEP,
+	// When in syscall-enter-stop, change the number of the system call to execute (only supported on arm)
+	// (it seems this has been removed from system headers by now)
+	//SET_SYSCALL = PTRACE_SET_SYSCALL,
+	/// Like SYSCALL, but do not execute the system call. Used for emulation applications like user-mode-Linux.
+	SYSEMU      = PTRACE_SYSEMU,
+	/// Like SYSEMU, but if there's no system call, then singlestep.
+	SYSEMU_SINGLESTEP = PTRACE_SYSEMU_SINGLESTEP,
+	/// Restart a stopped tracee, but let it enter a SIGSTOP like state. Works only for SEIZE'd tracees.
+	LISTEN      = PTRACE_LISTEN,
+	/// Send a SIGKILL to the tracee (this is buggy, don't use it).
+	KILL        = PTRACE_KILL,
+	/// Stop a tracee. Works only for SIZE'd tracees.
+	INTERRUPT   = PTRACE_INTERRUPT,
+	/// Attach to the specified process, making it a tracee of the caller.
+	ATTACH      = PTRACE_ATTACH,
+	/// Like ATTACH but does not stop the process.
+	SEIZE       = PTRACE_SEIZE,
+	/// Dump the tracee's classic BPF filters.
+	SECCOMP_GET_FILTER = PTRACE_SECCOMP_GET_FILTER,
+	/// Restart the stopped tracee, but first detach from it.
+	DETACH      = PTRACE_DETACH,
+	/// Performs an operation similar to `get_thread_area()`.
+	GET_THREAD_AREA = PTRACE_GET_THREAD_AREA,
+	/// Performs an operation similar to `set_thread_area()`.
+	SET_THREAD_AREA = PTRACE_SET_THREAD_AREA,
+	/// Retrieve information about the system call that cause the stop.
+	GET_SYSCALL_INFO = PTRACE_GET_SYSCALL_INFO,
+};
+
 } // end ns
